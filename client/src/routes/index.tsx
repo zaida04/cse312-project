51472da46c1a936e3e02b4eac4628e93a25ce44b
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-16 19:10:07 -0400

    feat: example for data fetching from frontend

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import { Route as rootRoute } from "./__root";
 import { createRoute } from '@tanstack/react-router';
+import { APIFetch } from "../util/fetcher";
 
 export const Route = createRoute({
     getParentRoute: () => rootRoute,
@@ -8,5 +10,15 @@ export const Route = createRoute({
 })
 
 function Index() {
+    // example of data fetching
+    useEffect(() => {
+        async function getData() {
+            const request = await APIFetch("GET", "/users/65f60caad6225df038c60445");
+            console.log(request);
+        }
+
+        getData();
+    }, []);
+
     return <div className="p-2">Hello from Index!</div>
 }
