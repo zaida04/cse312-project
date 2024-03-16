d0e845e42f54f90996c9af558f2f88d2cdeb6bea
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-16 17:30:14 -0400

    feat: util for retrieving from database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import User from "./db/models/User";
 import "./db/connect_mongo";
+
+import userRouter from './routes/users';
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(cors());
 app.use((req, res, next) => {
@@ -18,6 +19,8 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+app.use(userRouter);
 
 app.listen(PORT, async () => {
     console.log(`Server is running on port http://localhost:${PORT}.`);
