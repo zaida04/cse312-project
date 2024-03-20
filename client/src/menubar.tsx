fdc3ac9dddc5e05a125bb873ae7ce69bee0e4006
thangtri <thangtri@buffalo.edu>
2024-03-20 14:53:33 -0400

    3-20-24-2-53-PM

--- client/src/menubar.tsx
+++ client/src/menubar.tsx
@@ -1,2 +1,17 @@
+import { Component } from 'react';
 import "./menubar-style.css"
 
+class Menubar extends Component {
+    render() {
+        return(
+            <div id="menubar">
+            <p>Home</p>
+            <div id="user-div">
+            <button id="profile-button">Zaid</button>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Menubar
--- client/src/sidebar.tsx
+++ client/src/sidebar.tsx
@@ -5,11 +5,19 @@ class Sidebar extends Component {
     render() {
         return(
             <div id="sidebar">
-            <img src={logo}></img>
-            <p>Hello there</p>
-            <button>Launch a rocket</button>
-            <button>Home</button>
-            <button>Explore</button>
+            <img id="logo" src={logo}></img>
+            <div id="sidebar-button-container">
+            <button className="sidebar-button">Launch a rocket</button>
+            &nbsp;
+            <button className="sidebar-button">Home</button>
+            &nbsp;
+            <button className="sidebar-button">Explore</button>
+            &nbsp;
+            <button className="sidebar-button">Notificatiton</button>
+            &nbsp;
+            <button className="sidebar-button">Messages</button>
+            &nbsp;
+            </div>
             </div>
         );
     }
