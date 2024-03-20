9b512a9a712a5392f0022271b6039e8ea5dec4a8
Zaid Arshad <nico.03727+github@gmail.com>
2024-03-20 14:25:22 -0400

    chore: revert index stuff

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,27 +1,26 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { Route as rootRoute } from "./__root";
 import { createRoute } from '@tanstack/react-router';
-// import { APIFetch } from "../util/fetcher";
-import HomePage from "../component/HomePage";
+import { APIFetch } from "../util/fetcher";
 import '../app.css'
 
 
 export const Route = createRoute({
     getParentRoute: () => rootRoute,
     path: "/",
-    component: HomePage,
+    component: Index,
 })
 
-// function Index() {
-//     // example of data fetching
-//     useEffect(() => {
-//         async function getData() {
-//             const request = await APIFetch("GET", "/users/65f60caad6225df038c60445");
-//             console.log(request);
-//         }
+function Index() {
+    // example of data fetching
+    useEffect(() => {
+        async function getData() {
+            const request = await APIFetch("GET", "/users/65f60caad6225df038c60445");
+            console.log(request);
+        }
 
-//         getData();
-//     }, []);
+        getData();
+    }, []);
 
-//     return <div className="p-2">Hello from Index!</div>
-// }
+    return <div className="p-2">Hello from Index!</div>
+}
