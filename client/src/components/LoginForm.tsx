434c8b121b12a05b1f3a04328f3974b7c8669055
Nico <nico.03727+github@gmail.com>
2024-03-20 14:44:39 -0400

    Signup and login form (#4)
    * Create login and signup forms
    
    * feat: add signup and login forms

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,14 @@
+import InputWithLabel from "../component/InputWithLabel";
+
+export default function LoginForm() {
+    return <div className="card p-8 w-1/2">
+        <h2 className="font-bold mb-2">Login to an account</h2>
+
+        <form>
+            <InputWithLabel label="Username" type="text" id="username" />
+            <InputWithLabel label="Password" type="password" id="password" />
+            <button className="btn btn-primary mt-4" type="submit">Login</button>
+        </form>
+    </div>
+}
+
---
+++ client/src/components/SignupForm.tsx
@@ -0,0 +1,16 @@
+import InputWithLabel from "../component/InputWithLabel";
+
+export default function SignupForm() {
+    return <div className="card w-2/5">
+        <h2 className="font-bold mb-2">Create an account</h2>
+
+        <form>
+            <InputWithLabel label="Email" type="email" id="email" />
+            <InputWithLabel label="Username" type="text" id="username" />
+            <InputWithLabel label="Password" type="password" id="password" />
+            <InputWithLabel label="Confirm password" type="password" id="confirm-password" />
+            <button className="btn btn-primary mt-4" type="submit">Create account</button>
+        </form>
+    </div>
+}
+
