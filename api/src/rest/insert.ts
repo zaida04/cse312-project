8b5a48defc78651d620b52b31eb83bd6146578c7
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-21 05:24:33 -0400

    feat: starting work of auth

--- api/src/rest/insert.ts
+++ api/src/rest/insert.ts
@@ -6,19 +6,36 @@ interface CreateOptions {
     outputKey: string;
     inputFields?: string[];
     outputFields?: string[];
+    conditionCheck?: (request: Request) => Promise<[true, null] | [false, string]>;
+    additionalFields?: (request: Request) => Promise<Record<string, any>>;
 }
 export function createInsert(model: mongoose.Model<any>, options: CreateOptions) {
     return async (request: Request, response: Response) => {
+        if (options.conditionCheck) {
+            const [condition, message] = await options.conditionCheck(request);
+            if (!condition) {
+                return response.status(400).json({
+                    error: true,
+                    message
+                });
+            }
+        }
+
         try {
             const filteredBody = options.inputFields ? filterInput(request.body, options.inputFields) : request.body;
+            const additionalData = options.additionalFields ? await options.additionalFields(request) : {};
+
             // Create a new document in the database
-            const createdDocument = await model.create(filteredBody);
+            const createdDocument = await model.create({
+                ...filteredBody,
+                ...additionalData
+            });
             const filteredOutput = options.outputFields ? filterInput(createdDocument, options.outputFields) : createdDocument;
 
             // Respond with the created document
             return response.status(201).json({
                 error: false,
-                [options.outputKey]: createdDocument
+                [options.outputKey]: filteredOutput
             });
         } catch (error) {
             // Handle potential errors, such as validation errors or duplicate keys
