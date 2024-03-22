48563ed574bad0a64f7203a908747a9082e13da4
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-22 16:34:26 -0400

    feat: finish auth stuff

--- api/src/middleware/isAuth.ts
+++ api/src/middleware/isAuth.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import { createHash } from "crypto";
 import Session from "../db/models/Session";
 import User from "../db/models/User";
+import { generateHashedValue } from "../utils/hash";
 
 export const is_auth = async (req: Request, res: Response, next: NextFunction) => {
     const cookieToken = req.cookies.token;
     if (!cookieToken) return res.status(403).json({ error: { message: "Missing auth token." } });
 
-    const hashedToken = createHash("sha256").update(cookieToken).digest("hex");
+    const hashedToken = generateHashedValue(cookieToken);
     const session = await Session.findOne({ token: hashedToken });
     if (!session) return res.status(403).json({ error: "Invalid auth token." });
 
