087115db018cc4f6ae7b1315db0f1ec4a5cd5ced
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-22 16:44:33 -0400

    feat: prettier login/signup comps

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,14 +1,18 @@
 import InputWithLabel from "./InputWithLabel";
 
 export default function SignupForm() {
-    return <div className="card w-2/5">
+    return <div className="card w-[30rem] shadow-md p-8 bordered">
         <h2 className="font-bold mb-2">Create an account</h2>
 
         <form>
-            <InputWithLabel label="Email" type="email" id="email" />
-            <InputWithLabel label="Username" type="text" id="username" />
-            <InputWithLabel label="Password" type="password" id="password" />
-            <InputWithLabel label="Confirm password" type="password" id="confirm-password" />
+            <div className="flex flex-row gap-4">
+                <InputWithLabel label="Email" type="email" id="email" />
+                <InputWithLabel label="Username" type="text" id="username" />
+            </div>
+            <div className="flex flex-row gap-4">
+                <InputWithLabel label="Password" type="password" id="password" />
+                <InputWithLabel label="Confirm password" type="password" id="confirm-password" />
+            </div>
             <button className="btn btn-primary mt-4" type="submit">Create account</button>
         </form>
     </div>
