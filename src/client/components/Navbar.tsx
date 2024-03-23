cf1b381f338185dad7f9241b4ca5dba87027af4f
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-03-22 20:46:59 -0400

    feat: finally fix the auth stuff

---
+++ src/client/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { useUser } from "../state"
+
+export default function Navbar() {
+    return <div className="navbar border-b mb-4 bg-base-100 w-3/5">
+        <div className="flex-1">
+            <a className="btn btn-ghost text-xl">Y.com</a>
+        </div>
+
+        <div className="flex-none">
+            <Profile />
+        </div>
+    </div>
+}
+
+function Profile() {
+    const user = useUser();
+    if (user === null) return <span className="loading loading-spinner loading-lg" />
+    if (user === false) return <p>You are not logged in.</p>
+
+    return <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="/pfp.png" />
+            </div>
+        </div>
+        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a href="/api/logout">Logout</a></li>
+        </ul>
+    </div>
+}
---
+++ src/client/state.ts
@@ -0,0 +1,25 @@
+import { atom, useAtom } from "jotai"
+import { IUser } from "../server/db/models/User";
+import { useEffect } from "react";
+import { APIFetch } from "./util/fetcher";
+
+export const userAtom = atom<IUser | false | null>(null);
+
+export function useUser() {
+    const [user, setUser] = useAtom(userAtom);
+
+    useEffect(() => {
+        async function getCurrentUser() {
+            const request = await APIFetch<{ user: IUser }>("GET", "/users/@me");
+            if (request.error) {
+                setUser(false);
+            } else {
+                setUser(request.user);
+            }
+        }
+
+        getCurrentUser();
+    }, []);
+
+    return user;
+}
