3e897a30660a96791971ee6041f86a2c7a196ef1
thangtri <thangtri@buffalo.edu>
2024-03-24 16:49:48 -0400

    3-24-24-4-49-PM menubar sidebar components

---
+++ src/client/components/Menubar.tsx
@@ -0,0 +1,34 @@
+// // import { Component } from 'react';
+// // import "./menubar-style.css"
+
+// // class Menubar extends Component {
+// //     render() {
+// //         return(
+// //             <div id="menubar">
+// //             <p>Home</p>
+// //             <div id="user-div">
+// //             <button id="profile-button">Zaid</button>
+// //             </div>
+// //             </div>
+// //         );
+// //     }
+// // }
+
+// // export default Menubar
+
+// export default function Header(props: {
+//     pageName: string,
+// }) {
+//   return (
+//     <header className="bg-yellow-200 text-black py-4">
+//         <div className="container mx-auto">
+//             <div className='flex justify-between'>
+//                 <h1 className="text-2xl font-bold">{[props.pageName]}</h1>
+//                 <ul className='flex justify-end items-center font-bold space-x-5 tracking-wide'>
+//                 <li><a href='/login'>Login</a></li>
+//                 </ul>
+//             </div>
+//         </div>
+//     </header>
+//   )
+// }
---
+++ src/client/components/Sidebar.tsx
@@ -0,0 +1,110 @@
+
+// import logo from './ylogo.png'
+
+
+// class Sidebar extends Component {
+//     render() {
+//         return(
+//             <div id="sidebar" className="relative h-32 w-32">
+//             <div className="absolute inset-y-0 left-0 w-16">04</div>
+//             {/* <div id="sidebar"> */}
+//             <img id="logo" src={logo}></img>
+//             <div id="sidebar-button-container">
+//             <button className="sidebar-button">Launch a rocket</button>
+//             &nbsp;
+//             <button className="sidebar-button">Home</button>
+//             &nbsp;
+//             <button className="sidebar-button">Explore</button>
+//             &nbsp;
+//             <button className="sidebar-button">Notificatiton</button>
+//             &nbsp;
+//             <button className="sidebar-button">Messages</button>
+//             &nbsp;
+//             {/* </div> */}
+//             </div>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Sidebar
+
+// import { HeartIcon } from "lucide-react";
+// import { Home } from 'lucide-react';
+// import { Search } from 'lucide-react';
+// import { Bell } from 'lucide-react';
+// import { Mail } from 'lucide-react';
+// import { User } from 'lucide-react';
+
+
+
+// export default function Post(props: {
+//     username: string,
+//     title: string,
+//     body: string,
+//     likes: string
+// }) {
+
+//     return <>
+//         <div className="card card-bordered card-compact w-[40rem] bg-base-100 border-slate-400 shadow-xl">
+//             {/* call on the API to get the pic. */}
+//             {/* <figure><img src={Musken} alt="musk" /></figure> */}
+
+//             {/* call on the API to fetch the post text and likes */}
+//             <div className="card-body">
+//                 {/* User avatar with a placeholder icon */}
+//                 <div className="avatar">
+//                     <div className="rounded-full w-8 h-8">
+//                         <img src="/pfp.png" alt="User Avatar" />
+//                     </div>
+
+//                     {/* username of the post */}
+//                     <h3 className="card-title pl-3">{props.username}</h3>
+//                 </div>
+
+//                 <h2>{props.title}</h2>
+//                 {/* post text goes here */}
+//                 <p>{props.body}</p>
+
+//                 {/* like button */}
+//                 <div className="card-actions justify-end space-x-2 mt-4">
+//                     {props.likes}
+//                     <label className="swap">
+
+//                         {/* this hidden checkbox controls the state */}
+//                         <input type="checkbox" />
+
+//                         {/* like on icon */}
+//                         <HeartIcon className="swap-off fill-current w-[24px] h-[24px]" />
+
+//                         {/* like off icon */}
+//                         <HeartIcon className="swap-on fill-current w-[24px] h-[24px]" />
+//                     </label>
+//                 </div>
+//             </div>
+//         </div>
+//     </>
+// };
+
+{/* <div class="bg-orange-300 absolute h-screen w-60">
+  <div class="absolute inset-y-0 left-0 w-16">
+    <button className="sidebar-button bg-pink-200 w-10">Launch rockets</button>
+    <button className="sidebar-button">Home</button>
+    <button className="sidebar-button">Explore</button>
+  </div>
+</div>
+
+<div class="bg-yellow-200 absolute right-0 h-20 w-[calc(100%_-_15rem)] text-black py-4 px-4">
+        <div class="container mx-auto">
+            <div class='flex justify-between'>
+                <h1 class="text-2xl font-bold">Home</h1>
+                <ul class='flex justify-end items-center font-bold space-x-5 tracking-wide'>
+                <li><a href='/login'>Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+<!-- <div class="absolute bg-yellow-200 right-0 h-32 w-[calc(100%_-_10rem)]">
+  <div class="absolute inset-x-0 top-0 h-16">02</div>
+</div> --> */}
