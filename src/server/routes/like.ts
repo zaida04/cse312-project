1721cd88d9a7d5e13ca3543586e34618104694a4
YOO8282 <yaabdall@buffalo.edu>
2024-03-24 18:30:05 -0400

    added the like api route

---
+++ src/server/routes/like.ts
@@ -0,0 +1,30 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import Post from '../db/models/Post'; 
+import { auth_middleware } from '../middleware/auth';
+
+const router = Router();
+
+router.post('/api/posts/:postId/like', auth_middleware,(async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.postId;
+    const userId = req.user!._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+        return next(new Error('Post not found'));
+    }
+
+    const index = post.likes.findIndex((id: { toString: () => any; }) => id.toString() === userId.toString());
+
+    if (index === -1) {
+        post.likes.push(userId);
+    } else {
+        post.likes.splice(index, 1);
+    }
+
+    await post.save();
+
+    res.status(200).send(post);
+}));
+
+export default router;
